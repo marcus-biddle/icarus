4b38b25193e9683b0c6a466607830c86d32d1c6d
marcus-biddle <marcusbiddle16@gmail.com>
2023-10-26 15:20:21 -0700

    added google login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,74 +1,31 @@
-import { useRef } from 'react'
+import React from 'react'
+import { Camera } from './components/Camera'
 import './App.css'
-
-import * as tf from '@tensorflow/tfjs';
-import * as poseDetection from '@tensorflow-models/pose-detection';
-import Webcam from 'react-webcam';
-import { RendererCanvas2d } from './utilities/tensor/index.js';
-import React from 'react';
+import { useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
 
 function App() {
-  const webcamRef = useRef<Webcam>(null);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // First things should be a sign in/up page using google chrome
+  const googleLogin = useGoogleLogin({
+    // https://react-oauth.vercel.app/
+    flow: 'auth-code',
+    onSuccess: async (codeResponse) => {
+        console.log(codeResponse);
+        const tokens = await axios.post(
+            'http://localhost:3001/auth/google', {
+                code: codeResponse.code,
+            });
 
-  const detect = async (instance: poseDetection.PoseDetector) => {
-    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current?.video?.readyState === 4) {
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
-      
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
-
-      const pose = await instance.estimatePoses(video);
-      console.log('pose', pose[0]);
-
-      drawCanvas(pose, video, videoWidth, videoHeight, canvasRef);
-    }
-  }
-
-  const drawCanvas = (pose: poseDetection.Pose[], video: HTMLVideoElement, videoWidth: number, videoHeight: number, canvas: React.RefObject<HTMLCanvasElement>) => {
-      const canvasElement = canvas.current as HTMLCanvasElement;
-      canvasElement.width = videoWidth;
-      canvasElement.height = videoHeight;
-
-      const renderer = new RendererCanvas2d(canvas.current);
-
-      renderer.drawResult(pose[0])
-  }
-// https://codesandbox.io/s/posedetection-demo-fxy4h?file=/src/renderer_canvas2d.js
-  const runPoseDetection = async () => {
-    await tf.setBackend('webgpu');
-
-    const model = poseDetection.SupportedModels.BlazePose;
-    const detector = await poseDetection.createDetector(model, { runtime: 'tfjs' });
-
-    setInterval(() => {
-      detect(detector);
-    }, 1000);
-  };
-
-  runPoseDetection();
-
+        console.log(tokens);
+    },
+    onError: errorResponse => console.log(errorResponse),
+});
   return (
-    <>
-      <canvas ref={canvasRef} width={500} height={500} style={{ 
-        position: "absolute",
-        backgroundColor: 'grey',
-        top: 0,
-        left: 0,
-        zIndex: 10
-        }}/>
-      <Webcam ref={webcamRef} style={{ 
-        position: "absolute",
-        width: '500',
-        height: '500',
-        top: 0,
-        left: 0,
-        zIndex: 9
-        }}/>
-      <div id="scatter-gl-container"/>
-    </>
+    <div>
+      {false && <Camera />}
+      test
+      <button onClick={() => googleLogin()}>Google sign in</button>
+    </div>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { GoogleOAuthProvider } from '@react-oauth/google'; //https://github.com/MomenSherif/react-oauth#usegooglelogin-both-implicit--authorization-code-flow
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="<your_client_id>">
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>;
   </React.StrictMode>,
 )

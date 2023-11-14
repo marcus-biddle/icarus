6dd5e482bde10ef919c3bafcd54a91844a873cb4
marcus-biddle <marcusbiddle16@gmail.com>
2023-11-13 16:16:50 -0800

    pushup controller

--- server/controllers/Pushups.controller.js
+++ server/controllers/Pushups.controller.js
@@ -3,7 +3,42 @@ import Pushup from '../models/Pushup.model.js';
 import User from '../models/User.model.js';
 import jwt from 'jsonwebtoken';
 
-  const addPushups = async (req, res) => {
+const getPushupSchema = async (res, req) => {
+  // When user logs in for the first time, if pushup entry exists do nothing else create it
+  // log that the user joined.
+  const googleId = req.body.googleId;
+
+  if (!googleId) return res.status(400).json({ error: 'googleId and pushup Count is required' });
+  const decodedToken = await jwt.decode(googleId);
+
+  try {
+    const user = await User.findOne({ email: decodedToken.email });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const foundPushupSchema = await Pushup.find({ user: user._id });
+
+    if (foundPushupSchema) return res.status(200);
+
+    const result = await Pushup.create({
+      user: user._id,
+      total: 0,
+      entries: [],
+      experiencePointConversion: 0.1
+    })
+
+
+    await RecentChanges.create({
+      action: `${decodedToken.name} joined the site.`,
+    });
+
+    return res.status(201);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+  const patchAddPushups = async (req, res) => {
+    // update PushupSchema if exists with a new entry.
     const googleId = req.body.googleId;
     const pushupCount = req.body.pushupCount;
 
@@ -13,32 +48,35 @@ import jwt from 'jsonwebtoken';
     try {
         const user = await User.findOne({ email: decodedToken.email });
 
-        if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-        }
+        if (!user) return res.status(404).json({ error: 'User not found' });
 
-        const userId = user._id;
+        const existingPushupSchema = await Pushup.findOne({ user: userId });
 
-        const result = await Pushup.create({
-        user: userId, 
-        count: pushupCount,
-        });
+        if (!existingPushupSchema) return res.status(404).json({ error: 'PushupSchema error.' });
+
+        const newEntry = { count };
+
+        // Push the new entry to the entries array
+        existingPushupSchema.entries.push(newEntry);
+
+        // // Update the total field based on the sum of counts
+        // existingPushupSchema.total = existingPushupSchema.entries.reduce((sum, entry) => sum + entry.count, 0);
+
+        // Save the updated document
+        await existingPushupSchema.save();
 
         await RecentChanges.create({
           action: `${decodedToken.name} completed ${pushupCount} pushup(s).`,
         });
 
-        const updatedUser = await User.updateTotalPushups(userId, pushupCount);
-
-        console.log('Total pushups updated:', updatedUser);
-
-        return res.status(201).json(result);
+        return res.status(201);
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
   }
 
   const getAllPushups = async (req, res) => {
+    // This might be entirely incorrect now
     try {
       const pushups = await Pushup.aggregate([
         {

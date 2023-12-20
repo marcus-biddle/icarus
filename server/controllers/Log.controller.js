06638091736669cefd66025c6b335002f542013a
Mars <marcusbiddle16@gmail.com>
2023-12-19 20:01:12 -0800

    .

--- server/controllers/Log.controller.js
+++ server/controllers/Log.controller.js
@@ -2,7 +2,7 @@ import Logs from '../models/Log.model.js';
 
 const getAllLogs = async (req, res) => {
   try {
-    const result = await Logs.find({}).sort({ createdAt: 1 }).limit(10);
+    const result = await Logs.find({}).limit(10);
     return res.json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });

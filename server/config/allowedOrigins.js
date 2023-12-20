0759d7560d54e6c84bdb8a31cdcd1d656707babf
Mars <marcusbiddle16@gmail.com>
2023-12-19 16:26:43 -0800

    .

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,5 +1,6 @@
 export const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5173',
-    'https://icarus-competition.vercel.app'
+    'https://icarus-competition.vercel.app',
+    'https://icarus-server.onrender.com'
   ];

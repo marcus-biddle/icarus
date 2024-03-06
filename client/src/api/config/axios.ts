34527fc4e5677a852c42e85a064377df8801d3b5
Mars <marcusbiddle16@gmail.com>
2024-03-05 17:33:36 -0800

    .

--- client/src/api/config/axios.ts
+++ client/src/api/config/axios.ts
@@ -6,7 +6,7 @@ import axios from 'axios';
 export const createAxiosInstance = () => {
   // Create an instance of Axios with custom configuration
   const instance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://icarus-server.onrender.com/api',
     headers: {
       'Content-Type': 'application/json', 
     },
